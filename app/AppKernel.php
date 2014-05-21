0787bdb786b9452f1348ce1195867240f21761e2
Michael Antoine <mantoine@linagora.com>
2014-05-21 18:54:51 +0200

    Create bundle ManManuiaBundle.
    Change-Id: I77f792dcd3aa4a497f4c2e7d0f2beb2e9cb26992

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // MY OWN BUNDLE
+            new Man\Bundle\ManuiaBundle\ManManuiaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Man/Bundle/ManuiaBundle/ManManuiaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Man\Bundle\ManuiaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ManManuiaBundle extends Bundle
+{
+}
