25e628c4f2ba3c3ae3078578d9da97a9c53610fd
Michael Antoine <mantoine@linagora.com>
2014-06-11 12:37:05 +0200

    Add Menu management using js.
    Change-Id: I6e196018e22750da9a725ae78df3f8b689351fed

--- src/Man/Bundle/ManuiaBundle/Resources/public/js/manuia.js
+++ src/Man/Bundle/ManuiaBundle/Resources/public/js/manuia.js
@@ -3,18 +3,32 @@
  */
 
 $(document).ready(function() {
-	
-	$( window ).resize(function() {
-		console.debug('Resize' );
-	});
+    
+    $( window ).resize(function() {
+        if( $('#menu-dropdown').length ) {
+            $('#menu-dropdown').remove()
+        }
+    });
 
     $('.navbar-toggle').click(function(){
         var $this   = $(this), href
+        var $container = $this.closest('.l-container');
+        // get class of target menu
         var target  = $this.attr('data-target')
             || e.preventDefault()
             || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
-        var $target = $(target)
-        var toggle = $(this).attr('data-toggle')
+
+        var $target = null;
+        if( $('#menu-dropdown ' + target).length ) {
+            $target = $('#menu-dropdown ' + target)
+        } else {
+            $target = $this.parent().next(target)
+            var $clone = $target.clone();
+            $row = $clone.wrap("<div id='menu-dropdown' class='row menu-dropdown'></div>").parent();
+            $target = $row.find(target);
+            $container.append($row);
+        }
+
         $this.attr('data-toggle', '');
         if( $target.is( ":hidden" ) ){
             $target.slideDown();
